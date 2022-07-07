7c598b96bd6d3b15aa046521944cae03fa38e40d
josephatJ <josephatjulius24@gmail.com>
2022-07-07 13:50:15 +0300

    Add support to capture existing users

--- create_users.py
+++ create_users.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 #import json
 from requests.auth import HTTPBasicAuth
 #import requests
@@ -39,6 +40,18 @@ users_file_name = ''
 users_system_ids = []
 users_details = []
 
+
+existing_users = []
+existing_users_headers= ['Names', 'username', 'OU', 'Response']
+existing_users.append(existing_users_headers)
+
+async def save_existing_users(rows):
+    path = os.getcwd()
+    with open(path + '/folder/existing.csv', 'w', encoding='UTF8') as file_to_write:
+        writer = csv.writer(file_to_write)
+        for row in rows:
+            writer.writerow(row)
+
 async def main():
     server_access = await get_user_name_and_password()
     DEST_BASE_URL = server_access['url']
@@ -55,6 +68,17 @@ async def main():
     # headers['Authorization'] = 'Basic %s' %  userAndPass
 
     for count,user_row in enumerate(users_data, start=0):
+        roles = []
+        groups = []
+        for role_id in user_row[7].split(","):
+            roles.append({
+                "id": role_id.replace(" ","")
+            })
+        for group_id in user_row[9].split(","):
+            groups.append({
+                "id": group_id.replace(" ","")
+            })
+
         user = {
                 "id": users_system_ids[count],
                 "firstName": user_row[1],
@@ -62,30 +86,34 @@ async def main():
                 "email": "",
                 "userCredentials": {
                     "userInfo": { "id": users_system_ids[count] },
-                    "username": user_row[3],
+                    "username": user_row[3].replace(" ","").replace("  ",""),
                     "password": user_row[10],
-                    "userRoles": [ {
-                    "id": user_row[7]
-                    } ]
+                    "userRoles": roles
                 },
                 "organisationUnits": [ {
                     "id": user_row[11]
                 } ],
                 "dataViewOrganisationUnits":  [ {
-                    "id": user_row[11]
+                    "id": user_row[12]
                 } ],
-                "userGroups": [ {
-                    "id": user_row[9]
-                } ]
+                "userGroups":groups
             }
         # Check if user exist
+        print(json.dumps(user))
         exist_res = await check_if_user_exist(user, DEST_BASE_URL,username,password, headers)
         print(exist_res)
         if 'users' in exist_res and len(exist_res['users']) > 0:
-            # 
-            response = await update_user(exist_res['users'][0]['id'],user, DEST_BASE_URL,username,password, headers)
-            print('updated')
-            print(response)
+            # Save on excel the user details
+            # response = await update_user(exist_res['users'][0]['id'],user, DEST_BASE_URL,username,password, headers)
+            # print('updated')
+            # print(response)
+            user =[]
+            user.append(user_row[1])
+            user.append(user_row[3])
+            user.append(user_row[11])
+            user.append(json.dumps(exist_res['users'][0]))
+            existing_users.append(user)
+            save_response = await save_existing_users(existing_users)
         else:
             response = await create_user(user, DEST_BASE_URL,username,password, headers)
             print(response)
